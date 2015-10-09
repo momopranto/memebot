e8369c864cc37a2e5412b6be2bf0967c05a4a40d
momopranto <johncena.alamin@gmail.com>
2015-10-09 14:15:50 -0400

    v.v

--- meme-generator-api-test.py
+++ meme-generator-api-test.py
@@ -1,22 +1,20 @@
 import json, requests
 
-DEFAULT_IMG_HEIGHT = [{'height':200}]
-
 def list_memes():
     r = requests.get('http://api.imgflip.com/get_memes')
     memelist = r.json()['data']['memes']
-    for i, meme in enumerate(memelist):
+    for meme in memelist:
 	print meme['id'] + ': ' + meme['name']
 
 def create_meme(MID, t0, t1):
     payload = {
-	'username': 'XXXXXXX',
-	'password': 'XXXXXXX',
-	'template_id': 306319,
-	'text0': 'Top text',
-	'text0': 'bOTTOM text'
+	'username': 'XXXXXXXXXXX',
+	'password': 'XXXXXXXXXXX',
+	'template_id': MID,
+	'text0': t0,
+	'text1': t1,
     }
-    r = requests.post("https://api.imgflip.com/caption_image", data=payload)
+    r = requests.post("http://api.imgflip.com/caption_image", data=payload)
     print r.text
 
 #list_memes()
